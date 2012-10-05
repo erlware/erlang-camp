2e80bade56cc16c9f3a6984df254b36c7c2eecd9
martinjlogan <martinjlogan@gmail.com>
2012-10-05 04:24:14 -0500

    added in create tables answer key

---
+++ distributed_erlang/.create_tables/create_tables.erl
@@ -0,0 +1,44 @@
+-module(create_tables).
+
+-export([init_tables/0, insert_member/3, insert_project/2]).
+
+-record(member, {
+          id,
+          name
+         }).
+
+-record(project, {
+          title,
+          description
+         }).
+
+-record(contributor, {
+          member_id,
+          project_title
+         }).
+
+init_tables() ->
+    mnesia:create_table(member,
+       [{attributes, record_info(fields, member)}]),
+    mnesia:create_table(project,
+       [{attributes, record_info(fields, project)}]),
+    mnesia:create_table(contributor,
+       [{type, bag}, {attributes, record_info(fields, contributor)}]).
+
+insert_member(Id, Name, ProjectTitles) when ProjectTitles =/= [] ->
+    User = #member{id = Id, name = Name},
+    Fun = fun() ->
+            mnesia:write(User),
+            lists:foreach(
+              fun(Title) ->
+                [#project{title = Title}] = mnesia:read(project, Title),
+                mnesia:write(#contributor{member_id = Id,
+                                          project_title = Title})
+              end,
+              ProjectTitles)
+          end,
+    mnesia:transaction(Fun).
+
+insert_project(Title, Description) ->
+    mnesia:dirty_write(#project{title = Title,
+                                description = Description}).
