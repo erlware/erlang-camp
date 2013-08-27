20cceeeefa5f870a9bf24802c3dfdea5e5c3f227
martinjlogan <martinjlogan@gmail.com>
2013-08-27 14:12:21 -0700

    added in recursion lessons

---
+++ essentials/ec_recur.erl
@@ -0,0 +1,32 @@
+%%% @author Billy Erlware
+%%% @doc a function that sums a sequence.
+%%% @copyright Erlware 2013
+-module(ec_recur).
+-export([sum_seq/1, sum_seq_tail/1, run_baby_run/1]). 
+
+%% @doc sum a sequence of a number to 1.
+-spec(sum_seq(number()) -> number()).
+sum_seq(1) -> 
+	1;
+sum_seq(N) ->
+	N + sum_seq(N - 1).
+
+%% @doc sum a sequence of a number to 1.
+-spec(sum_seq_tail(number()) -> number()).
+sum_seq_tail(N) ->
+    sum_seq(N, 0).
+
+sum_seq(0, Acc) ->
+	Acc;
+sum_seq(N, Acc) -> 
+	sum_seq(N - 1, N + Acc). 
+
+%% Tail recursive
+-spec(run_baby_run(number()) -> number()).
+run_baby_run(N) ->
+	run_baby_run(N, "").
+
+run_baby_run(0, Acc) ->
+	Acc;
+run_baby_run(N, Acc) ->
+	run_baby_run(N - 1, Acc ++ ".").
