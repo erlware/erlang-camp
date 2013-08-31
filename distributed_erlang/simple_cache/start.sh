9fe814b85347e4ce40a156404c781f0ce35bce01
martinjlogan <martinjlogan@gmail.com>
2013-08-31 04:02:24 -0700

    echo server from dist programming lesson

--- distributed_erlang/simple_cache/start.sh
+++ distributed_erlang/simple_cache/start.sh
@@ -4,4 +4,4 @@ cd `dirname $0`
 NAME=$1
 shift
 
-exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -sname $NAME -setcookie rd -s resource_discovery start -config ebin/sys $@
+exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name $NAME -s resource_discovery start -config ebin/sys $@
