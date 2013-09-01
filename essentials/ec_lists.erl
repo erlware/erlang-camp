fd38466b3c563a9e64f0017ae1373b8bc9c1226a
martinjlogan <martinjlogan@gmail.com>
2013-09-01 14:02:43 -0700

    updated along with latest prezo

--- essentials/ec_lists.erl
+++ essentials/ec_lists.erl
@@ -1,7 +1,7 @@
 %%% @doc a bit of higher order functional goodness
 
 -module(ec_lists).
--export([remove_odd/1, yourmap/2]).
+-export([remove_odd/1, twomult/1, yourmap/2]).
 
 %% @doc remove odd elements from a list of integers
 -spec remove_odd(list()) -> list().
@@ -15,6 +15,13 @@ remove_odd([H|T], Acc) when H rem 2 == 0 ->
 	remove_odd(T, [H|Acc]);
 remove_odd([_H|T], Acc) ->
 	remove_odd(T, Acc).
+
+%% @doc multiply each integer in the given list by 2
+-spec twomult(list()) -> list().
+twomult([H|T]) ->
+	[H * 2|twomult(T)];
+twomult([]) -> 
+	[].
 
 %% @doc multiply each element by 2
 -spec yourmap(fun(), list()) -> list().
