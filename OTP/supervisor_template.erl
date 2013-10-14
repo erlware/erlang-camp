2a557e2f3b1634f9dd3e1f0e42d788153ffcdf5b
martinjlogan <martinjlogan@gmail.com>
2013-10-13 18:45:14 -0700

    added cheatsheets for my projects

---
+++ OTP/supervisor_template.erl
@@ -0,0 +1,48 @@
+%%% @author You
+%%% @copyright (C) 2013, Erlware
+%%% @doc
+
+-module(supervisor_template).
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+-define(IMPLEMENTATION_MODULE, ?MODULE).
+
+%%%======================================================================
+%%% API
+%%%======================================================================
+
+%% @doc Starts the supervisor.
+-spec start_link() -> {ok, pid()} | ignore | {error, term()}
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?IMPLEMENTATION_MODULE, []).
+
+
+%%%======================================================================
+%%% Behaviour Callback Functions
+%%%======================================================================
+init([]) ->
+    RestartStrategy = one_for_one,
+    MaxRestarts = 1000,
+    MaxSecondsBetweenRestarts = 3600,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    Restart = permanent,
+    Shutdown = 2000,
+    Type = worker,
+
+    AChild = {module_name, {module_name, start_link, []},
+              Restart, Shutdown, Type, [module_name]},
+
+    {ok, {SupFlags, [AChild]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
